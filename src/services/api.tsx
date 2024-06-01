2a72761c687ecf46eb2481617cc7bbb95e8d8521
Sidnei Farias <sidneijr.77@gmail.com>
2024-06-01 15:58:42 -0300

    fix(services): remove incorrect typing

--- src/services/api.tsx
+++ src/services/api.tsx
@@ -1,40 +1,5 @@
 import { env } from '@/env'
 
-type DataTypes = {
-  login: string
-  id: number
-  node_id: string
-  avatar_url: string
-  gravatar_id: string
-  url: string
-  html_url: string
-  followers_url: string
-  following_url: string
-  gists_url: string
-  starred_url: string
-  subscriptions_url: string
-  organizations_url: string
-  repos_url: string
-  events_url: string
-  received_events_url: string
-  type: string
-  site_admin: boolean
-  name: string
-  company: null
-  blog: string
-  location: string
-  email: null
-  hireable: boolean
-  bio: string
-  twitter_username: null
-  public_repos: number
-  public_gists: number
-  followers: number
-  following: number
-  created_at: Date
-  updated_at: Date
-}
-
 /**
  * A fetch API wrapper.
  *
@@ -52,7 +17,7 @@ export async function api(path: string, init?: RequestInit) {
 
   try {
     const response = await fetch(url, init)
-    const data = (await response.json()) as DataTypes
+    const data = await response.json()
 
     if (!response.ok) {
       throw new Error('Fetch was unsuccessfull. Is the query parameter valid?')
