f043e5f5a965b0169369c931e53e86ed919b6dca
Sidnei Farias <sidneijr.77@gmail.com>
2024-06-05 14:33:02 -0300

    feat(nav): add description to menuItens

--- src/components/nav/nav-menu.tsx
+++ src/components/nav/nav-menu.tsx
@@ -10,24 +10,30 @@ import {
   NavigationMenuList,
 } from '@/components/ui/navigation-menu'
 
+import { Small } from '../typography/small'
+
 const menuItens = [
   {
     name: 'Home',
+    description: 'About me',
     slug: '/',
     icon: <Home height={20} width={20} />,
   },
   {
     name: 'Portfolio',
+    description: 'My projects',
     slug: '/portfolio',
     icon: <Folders height={20} width={20} />,
   },
   {
     name: 'Blog',
+    description: 'My posts',
     slug: '/blog',
     icon: <BookOpenText height={20} width={20} />,
   },
   {
-    name: 'Contato',
+    name: 'Contact',
+    description: "Let's talk",
     slug: '/contact',
     icon: <Mail height={20} width={20} />,
   },
@@ -40,20 +46,35 @@ export function NavMenu() {
     <NavigationMenu className="nav-menu flex flex-1 items-start justify-normal">
       <NavigationMenuList className="flex w-full flex-col items-start gap-2">
         {menuItens.map((item) => {
-          const { name, slug, icon } = item
+          const { name, description, slug, icon } = item
+
+          /**
+           * Avoids multiple active items due to home having a '/' slug, which is a character also present in all other slugs.
+           * The "startsWith" method does a complete comparison of a string, while "includes" verifies each character of a string, causing the issue described above.
+           */
+          const isActive =
+            slug === '/' ? pathname === slug : pathname.startsWith(slug)
 
           return (
             <NavigationMenuItem
               key={crypto.randomUUID()}
-              className={`!mx-0 block w-full rounded transition-colors hover:bg-white ${pathname === slug ? 'bg-white text-black-200' : ''}`}
+              className={`!mx-0 block w-full rounded transition-colors hover:bg-white ${isActive ? 'bg-white text-black-200' : ''}`}
             >
               <Link
-                className="flex flex-1 items-start justify-start gap-2 p-2 transition-colors hover:text-black-200"
+                className="flex flex-1 flex-col items-start justify-start p-2 transition-colors hover:text-black-200"
                 href={slug}
               >
-                {icon}
+                <div className="flex gap-2">
+                  {icon}
 
-                {name}
+                  <span className={`${pathname === slug && 'font-medium'}`}>
+                    {name}
+                  </span>
+                </div>
+
+                {pathname === slug && (
+                  <Small className="h-auto pl-7">{description}</Small>
+                )}
               </Link>
             </NavigationMenuItem>
           )
